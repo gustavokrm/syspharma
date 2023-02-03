b4572b552232531959681d94f2a046cf43ff1544
gustavokrm <gustavokrm@gmail.com>
2023-02-03 15:46:30 -0300

    Initial commit.

---
+++ syspharma/src/test/java/com/camaratapira/syspharma/SyspharmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camaratapira.syspharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyspharmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
