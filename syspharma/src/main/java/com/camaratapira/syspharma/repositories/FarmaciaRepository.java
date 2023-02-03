fc9b9b30d59ebeac242919bee26bccaf2381d49a
gustavokrm <gustavokrm@gmail.com>
2023-02-03 16:11:18 -0300

    Added even more commentaries.

--- syspharma/src/main/java/com/camaratapira/syspharma/repositories/FarmaciaRepository.java
+++ syspharma/src/main/java/com/camaratapira/syspharma/repositories/FarmaciaRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.repository.CrudRepository;
 import com.camaratapira.syspharma.entity.Farmacia;
 
 public interface FarmaciaRepository extends CrudRepository<Farmacia, Integer> {
+	
+	//o nome das interfaces deve ser igual ao das variaveis
 	List<Farmacia> findByidfarmacia(int idfarmacia);
 	List<Farmacia> findBynomefarmacia(String nomefarmacia);
 
