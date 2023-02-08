eae68e6c68b4dd192dd0f728bcfe6800739188ea
gustavokrm <gustavokrm@gmail.com>
2023-02-08 16:30:21 -0300

    Finished the delete Mapping for the servidor entity.

--- syspharma/src/main/java/com/camaratapira/syspharma/controller/ServidorController.java
+++ syspharma/src/main/java/com/camaratapira/syspharma/controller/ServidorController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.camaratapira.syspharma.entity.Farmacia;
 import com.camaratapira.syspharma.entity.Funcao;
 import com.camaratapira.syspharma.entity.Servidor;
 import com.camaratapira.syspharma.repositories.FuncaoRepository;
@@ -60,11 +61,23 @@ public class ServidorController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 	
-	@PostMapping("/inserirservidor")
-	public ResponseEntity<Servidor> createServidor(@RequestBody Servidor servidor){
+	/*@PostMapping("/criarservidor")
+	public ResponseEntity<Servidor> createServidor(@PathVariable("servidor") int idservidor,
+			@RequestBody Servidor servidor){
 		try {
 			Servidor _servidor = servidorRepository.save(new Servidor(servidor.getNomeservidor(), servidor.getCpf(), servidor.getRg(), servidor.getSalario(), servidor.isAtivo()));
 			return new ResponseEntity<>(_servidor, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+	}*/
+	
+	@DeleteMapping("/deletarservidor/{idservidor}") /* é preciso adicionar a anotação @RequestBody para que ele possa achar o idfarmacia */
+	public ResponseEntity<HttpStatus> deleteServidor(@PathVariable("idservidor") int idservidor, @RequestBody Servidor servidor){
+		try {
+			servidorRepository.deleteById(idservidor);
+			return new ResponseEntity<>(HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
