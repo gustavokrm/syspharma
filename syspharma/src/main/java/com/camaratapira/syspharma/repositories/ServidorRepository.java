9f783df8ab09377b6390f27299b9fae32186dbd3
gustavokrm <gustavokrm@gmail.com>
2023-02-09 14:50:15 -0300

    Clean up of the code

--- syspharma/src/main/java/com/camaratapira/syspharma/repositories/ServidorRepository.java
+++ syspharma/src/main/java/com/camaratapira/syspharma/repositories/ServidorRepository.java
@@ -1,15 +1,8 @@
 package com.camaratapira.syspharma.repositories;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
-import com.camaratapira.syspharma.entity.Funcao;
 import com.camaratapira.syspharma.entity.Servidor;
-
-import jakarta.transaction.Transactional;
 
 public interface ServidorRepository extends JpaRepository<Servidor, Integer>{
 	List<Servidor> findByidservidor(int idservidor);
