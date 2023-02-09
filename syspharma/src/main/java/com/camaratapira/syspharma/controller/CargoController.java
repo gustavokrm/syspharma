5d00ed6d89c043f923c5e59f39372989b564553e
gustavokrm <gustavokrm@gmail.com>
2023-02-09 14:35:51 -0300

    Finished basic work on inserting info about employees.

--- syspharma/src/main/java/com/camaratapira/syspharma/controller/CargoController.java
+++ syspharma/src/main/java/com/camaratapira/syspharma/controller/CargoController.java
@@ -4,15 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.camaratapira.syspharma.entity.Cargo;
-import com.camaratapira.syspharma.entity.Funcao;
 import com.camaratapira.syspharma.repositories.CargoRepository;
 
 @RestController
@@ -22,7 +26,7 @@ public class CargoController {
 	@Autowired
 	CargoRepository cargoRepository;
 	
-	@GetMapping("/cargo")
+	@GetMapping("/cargo/listartodos")
 	public ResponseEntity<List<Cargo>> getAllCargos(@RequestParam(required=false)String descricaocargo){
 		List<Cargo> cargo = new ArrayList<Cargo>();
 		
@@ -39,7 +43,7 @@ public class CargoController {
 		
 	}
 	
-	@GetMapping("/listacargos/{idcargo}")
+	@GetMapping("/cargo/listacargos/{idcargo}")
 	public ResponseEntity<Cargo> getcargoById(@PathVariable("idcargo") int idcargo){
 		Optional<Cargo> cargoData = cargoRepository.findById(idcargo);
 		
@@ -49,5 +53,42 @@ public class CargoController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 	
+	@PostMapping("/cargo/criarcargos")
+	public ResponseEntity<Cargo> createCargo(@RequestBody Cargo cargo){
+		try {
+			Cargo _cargo = cargoRepository.save(cargo);
+			return new ResponseEntity<>(_cargo, HttpStatus.CREATED);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return new ResponseEntity<Cargo>(null, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+	}
 	
+	@PutMapping("/cargo/atualizarcargos")
+	public ResponseEntity<Cargo> updateCargo(@PathVariable("idcargo") int idcargo, @RequestBody Cargo cargo){
+		Optional<Cargo> cargoData = cargoRepository.findById(idcargo);
+		if(cargoData.isPresent()){
+			Cargo _cargo = cargoData.get();
+			_cargo.setDescricaocargo(cargo.getDescricaocargo());
+			return new ResponseEntity<>(cargoRepository.save(_cargo), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+	@DeleteMapping("/cargo/deletarcargo/{idcargo}")
+	public ResponseEntity<HttpStatus> deleteCargo(@PathVariable("idcargo") int idcargo, @RequestBody Cargo cargo){
+		try {
+			cargoRepository.deleteById(idcargo);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	} 
+		
 }
